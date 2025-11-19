a7dfb6a69bbeb0ea8cfa90a66bd2a5b1a9d4ad18
GojoSatoru1738 <ajmiller@email.neit.edu>
2025-11-18 23:42:38 -0500

    Update memoryButton.js

--- memoryButton.js
+++ memoryButton.js
@@ -2,69 +2,46 @@ import { Toolbox } from "./toolbox.js";
 
 export class MemoryButton {
 
-    x = 50;
-    y = 50;
-    color;
-    width = 100;
-    height = 100;
-    canvas;
-    pencil;
-    isFaceUp = false; 
-    toolbox = new Toolbox();
-
-    constructor(canvas, pencil, x, y, color) {
+    constructor(canvas, pencil, x, y, color, flipCallback) {
         this.x = x;
         this.y = y;
         this.color = color;
-        this.pencil = pencil;
+        this.width = 100;
+        this.height = 100;
         this.canvas = canvas;
+        this.pencil = pencil;
+        this.toolbox = new Toolbox();
+        this.isFaceUp = false;
+        this.flipCallback = flipCallback;
+
         canvas.addEventListener("click", (e) => this.onClick(e));
     }
 
- draw() {
-    if (this.isFaceUp) {
-      
-        this.pencil.fillStyle = this.color;
-        this.pencil.fillRect(this.x, this.y, this.width, this.height);
-    } else {
-       
-        this.pencil.fillStyle = "#444";
-        this.pencil.fillRect(this.x, this.y, this.width, this.height);
-
-        this.pencil.fillStyle = "white";
-        this.pencil.font = "50px Times New Roman";
-        this.pencil.fillText("?", this.x + 35, this.y + 60);
+    draw() {
+        if (this.isFaceUp) {
+            this.pencil.fillStyle = this.color;
+            this.pencil.fillRect(this.x, this.y, this.width, this.height);
+        } else {
+            this.pencil.fillStyle = "#444";
+            this.pencil.fillRect(this.x, this.y, this.width, this.height);
+            this.pencil.fillStyle = "white";
+            this.pencil.font = "50px Times New Roman";
+            this.pencil.fillText("?", this.x + 35, this.y + 60);
+        }
     }
-}
 
-    
     onClick(event) {
         let clickX = event.offsetX;
         let clickY = event.offsetY;
 
-        let isClickInButton = this.toolbox.isWithinRect(
+        let inside = this.toolbox.isWithinRect(
             clickX, clickY, this.x, this.y, this.width, this.height
         );
 
-        if(isClickInButton) {
-            this.isFaceUp = !this.isFaceUp;
-        }
+        if (!inside) return;
+        if (this.isFaceUp) return;
+
+        this.isFaceUp = true;
+        this.flipCallback(this);   // tells the main game a flip happened
     }
-
-    flip() {
-    if (this.isFaceUp) return;
-
-    this.isFaceUp = true;
-    flippedCards.push(this);
-
-    if (flippedCards.length === 2) {
-        checkMatch();
-    }
-}
-
-
-    
-
-
-
 }
